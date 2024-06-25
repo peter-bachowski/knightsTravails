1c9cda7a108589f8f7b37432f8705a9617aa0bde
Peter <bachowskip@gmail.com>
2024-06-25 15:08:30 -0700

    Update the algo

--- main.js
+++ main.js
@@ -3,86 +3,69 @@ class ChessBoard {
         this.boardArray = boardArray;
         this.knights = knights;
         let alphabet = 'ABCDEFGH';
-        for (let horz = 1; horz <= 8; horz++) {
-            for (let vert = 1; vert <= 8; vert ++) {
-                let newChessTile = {
-                    position: [horz, vert],
-                    passed: false,
-                    id: alphabet.charAt(vert - 1) + horz
-                }
-                boardArray.push(newChessTile);
-            }
-        }
     }
 
     addKnight (initPos) { // creates a knight on the chessboard at the deside location, and creates a tree for all possible moves
         const newKnight = new Knight(initPos);
         newKnight.root = new Node(initPos);
         this.knights.push(newKnight);
-        for (let i = 0; i < this.boardArray.length; i++) {
-            let boardTileObj = this.boardArray[i];
-            if (boardTileObj.position === initPos) {
-                boardTileObj.passed = true;
-            }
-        }
     }
 
-    knightMoves (currPos, finalPos) {
-
+    knightMoves (initPos, finalPos) {
+        let root = new Node(initPos);
+        root.nextMoves = this.createPossibleMoves(root);
+        root.nextMoves.forEach(element => {
+            element.nextMoves = this.createPossibleMoves(element);
+        });
     }
 
-    possibleMoves (position) {
 
-        let possibleMovesArray = [];
-        let moveArray = [];
+
+    createPossibleMoves (root) {
+
+        let moveArray = []; 
+        let possibleMovesArray = []; //this array should have nodes
+        const currPos = root.currPos;
     
-        moveArray.push([position[0]+1, position[1]+2]);
-        moveArray.push([position[0]+1, position[1]-2]);
-        moveArray.push([position[0]-1, position[1]+2]);
-        moveArray.push([position[0]-1, position[1]-2]);
-        moveArray.push([position[0]+2, position[1]+1]);
-        moveArray.push([position[0]+2, position[1]-1]);
-        moveArray.push([position[0]-2, position[1]+1]);
-        moveArray.push([position[0]-2, position[1]-1]);
+        moveArray.push([currPos[0]+1, currPos[1]+2]);
+        moveArray.push([currPos[0]+1, currPos[1]-2]);
+        moveArray.push([currPos[0]-1, currPos[1]+2]);
+        moveArray.push([currPos[0]-1, currPos[1]-2]);
+        moveArray.push([currPos[0]+2, currPos[1]+1]);
+        moveArray.push([currPos[0]+2, currPos[1]-1]);
+        moveArray.push([currPos[0]-2, currPos[1]+1]);
+        moveArray.push([currPos[0]-2, currPos[1]-1]);
     
-        for (let i = 1; i <= 8; i++) {
-            let element = moveArray[i-1];
-    
+        for (let i = 0; i < 8; i++) { //the loop checks if there are any negative coordinates in the move array
+            let element = moveArray[i];
             if (element[0] < 0 || element[1] < 0 || element[0] > 8|| element[1] > 8) {
                 continue;
             }
             else {
-                for (let i = 0; i < this.boardArray.length; i++) {
-                    let boardTile = this.boardArray[i];
-                    if (element === boardTile.position && boardTile.passed === false) {
-                        let newNode = new Node(element);
-                        newNode.nextMoves = this.possibleMoves(element);
-                        possibleMovesArray.push(newNode);
-                        boardTile.passed = true;
-                        break;
-                    }
-                }
+                possibleMovesArray.push(new Node(element));
             }
-        }
-    
+        }    
         return possibleMovesArray;
     }
 }
 
 class Node {
-    constructor (position, nextMoves = []) {
+    constructor (position, nextMoves = []) { //first parameter is a coordinate, second parameter is an array of nodes
         this.position = position;
         this.nextMoves = nextMoves;
     }
 }
 
 class Knight {
-    constructor (currPos, root = null) {
+    constructor (currPos, root) {
         this.currPos = currPos;
         this.root = root;
     }
+
+    
 }
 
-const newBoard = new ChessBoard();
-newBoard.addKnight([5,4]);
-console.log(newBoard);
+const board1 = new ChessBoard();
+board1.addKnight([0,0]);
+const knight1 = board1.knights[0];
+console.log(knight1);
